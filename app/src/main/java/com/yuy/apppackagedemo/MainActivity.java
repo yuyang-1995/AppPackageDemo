7a439b14a84174170814c27ddedbeafc001105c1
yuy <yuyang_keep@163.com>
2019-09-03 22:10:05 +0800

    Initial commit

---
+++ app/src/main/java/com/yuy/apppackagedemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.yuy.apppackagedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String channel = getChannel();
+        Toast.makeText(MainActivity.this,channel, Toast.LENGTH_SHORT).show();
+    }
+
+    private String getChannel() {
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString("UMENG_CHANNEL");
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return "";
+    }
+}
